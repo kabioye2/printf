00bca31d19ca57d7483087a8b3d6088f49fc3f1e
Kehinde Abioye <kabioye2@gmail.com>
2023-09-19 21:31:02 -0700

    added addtl func, fixed int print

--- 0-printf.c
+++ 0-printf.c
@@ -3,6 +3,46 @@
 #include <stdarg.h>
 
 /**
+ * _printfInt - produce output for integers
+ * @d_i: integer
+ * Return: the number of characters printed
+ */
+
+int _printfInt(int d_i)
+{
+	char buffer[12];
+	int temp;
+	int digits;
+	int index;
+
+	digits = 0;
+	index = 0;
+
+	if (d_i < 0)
+	{
+		_putchar('-');
+		d_i = -d_i;
+	}
+	temp = d_i;
+	while (temp != 0)
+	{
+		temp /= 10;
+		digits++;
+	}
+
+	while (d_i > 0)
+	{
+		buffer[index++] = '0' + (d_i % 10);
+		d_i /= 10;
+	}
+	while (index > 0)
+	{
+		_putchar(buffer[--index]);
+	}
+	return (digits);
+}
+
+/**
  * _printf - produces output according to a format
  * @format: a format character string that handles output format
  * Return: the number of characters printed
@@ -11,15 +51,12 @@
 int _printf(const char *format, ...)
 {
 	int i;
-	int j;
 	char c;
 	char *s;
 	int d_i;
-	int digits;
-	int temp;
 	va_list args;
 
-	digits = 0;
+	i = 0;
 	va_start(args, format);
 
 	while (*format != '\0')
@@ -46,23 +83,7 @@ int _printf(const char *format, ...)
 			else if (*format == 'd' || *format == 'i')
 			{
 				d_i = va_arg(args, int);
-				if (d_i < 0)
-				{
-					_putchar('-');
-					d_i = -d_i;
-				}
-				temp = d_i;
-				while (temp != 0)
-				{
-					temp /= 10;
-					digits++;
-				}
-
-				for (j = 0; j < digits; j++)
-				{
-					_putchar('0' + (d_i % 10));
-					d_i /= 10;
-				}
+				i += _printfInt(d_i);
 			}
 			else if (*format == '%')
 			{
@@ -72,7 +93,7 @@ int _printf(const char *format, ...)
 			else
 			{
 				_putchar('%');
-				putchar(*format);
+				_putchar(*format);
 				i += 2;
 			}
 		}
